04a327d8deae9f1f9d8ace3846e4e2de37dde0ec
acrix <tehday@yandex.ru>
2021-01-11 09:52:54 +0300

    init commit 1.0

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 
 
-if ('serviceWorker' in navigator)
-{
-  navigator.serviceWorker.register('sw.js').then( (registration)=> {
-    console.log('Service worker registration successful, scope is:', registration.scope);
-  })
-  .catch( (error)=> {
-    console.log('Service worker registration failed, error:', error);
-  });
-}
-
 $(document).ready(function() {
   console.log("DOM ready");
+  
+  if ('serviceWorker' in navigator)
+  {
+    navigator.serviceWorker.register('sw.js').then( (registration)=> {
+      console.log('Service worker registration successful, scope is:', registration.scope);
+    })
+    .catch( (error)=> {
+      console.log('Service worker registration failed, error:', error);
+    });
+  }
+
 });
 
 $(document).on("mobileinit", function() {
