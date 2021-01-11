5ee2b3219f2921e5af0c575eac31ffe10167a2ed
acrix <tehday@yandex.ru>
2021-01-11 07:30:24 +0300

    init commit 1.0

--- sw.js
+++ sw.js
@@ -61,7 +61,7 @@ self.addEventListener('fetch', (event)=> {
   event.respondWith(
     caches.match(event.request).then( (response)=> {
       return response || fetch(event.request).then( (response)=> { return response; } );
-    });
+    })
   );
 });
 
